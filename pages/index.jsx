cbbe66a0bd11b853766ae948dcd8a0e6e65eb629
unknown <khalidnabgaoui4@gmail.com>
2024-07-31 23:10:58 +0100

    setup db and login with google logic

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,22 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+
 export default function Home() {
-  return (
-    <div className="bg-blue-900 w-screen h-screen flex items-center">
-      <div className="text-center w-full">
-        <button className="bg-white p-2 px-4 rounded-md">
-          Login With Google
-        </button>
+  const { data: session } = useSession();
+
+  if (!session) {
+    return (
+      <div className="bg-blue-900 w-screen h-screen flex items-center">
+        <div className="text-center w-full">
+          <button
+            className="bg-white p-2 px-4 rounded-md"
+            onClick={() => signIn("google")}
+          >
+            Login With Google
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+
+  return <div>Logged in as {session.user.name}!</div>;
 }
