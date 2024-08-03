a3d85d8e245ba8bf12e9022c07aa7eb9cb3e25c3
unknown <khalidnabgaoui4@gmail.com>
2024-08-03 17:26:55 +0100

    admin security

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -9,6 +9,11 @@ export default function Nav() {
   const router = useRouter();
   const { pathname } = router;
 
+  async function logout() {
+    await router.push("/");
+    await signOut();
+  }
+
   return (
     <aside className="text-white p-4 pr-0">
       <Link href={"/"} className="flex gap-1 mb-4 mr-4">
@@ -148,7 +153,7 @@ export default function Nav() {
           Settings
         </Link>
 
-        <button onClick={() => signOut()} className={inactiveLink}>
+        <button onClick={logout} className={inactiveLink}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,9 +1,11 @@
-import clientPromise from "@/lib/mongodb";
-import { MongoDBAdapter } from "@auth/mongodb-adapter";
-import NextAuth from "next-auth";
+import NextAuth, { getServerSession } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "@/lib/mongodb";
 
-export default NextAuth({
+const adminEmails = ["khalidnabgaoui4@gmail.com"];
+
+export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
@@ -11,4 +13,25 @@ export default NextAuth({
     }),
   ],
   adapter: MongoDBAdapter(clientPromise),
-});
+
+  callbacks: {
+    session: ({ session, token, user }) => {
+      if (adminEmails.includes(session?.user?.email)) {
+        return session;
+      }
+      return false;
+    },
+  },
+};
+
+export default NextAuth(authOptions);
+
+export async function isAdminRequest(req, res) {
+  const session = await getServerSession(req, res, authOptions);
+
+  if (!adminEmails.includes(session?.user?.email)) {
+    res.status(401);
+    res.end();
+    throw "Not an admin";
+  }
+}
